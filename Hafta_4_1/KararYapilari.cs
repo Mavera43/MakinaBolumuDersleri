889404db5e563ffe4fd7ccc3ebb967d34f3c262c
Şenol Demirci <dsenol@hotmail.com>
2022-03-11 09:49:30 +0300

    Proje dosyası ekle.

---
+++ Hafta_4_1/KararYapilari.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta_4_1
+{
+    class KararYapilari
+    {
+        static void Main(string[] args)
+        {
+            ternaryKullanımı();
+
+
+            // ifelse4();
+            // ifelse3();
+            // ifelse2();
+            // ifelse1();
+        }
+
+        private static void ternaryKullanımı()
+        {
+            int sayı = 5 + 1;
+            //ternary operatörü kullanımı. tek satırlık if else yapısı.
+
+            //tek bir satırda kullanılabilir
+            Console.WriteLine((sayı % 2 == 0) ? "Sayı Çift" : "Sayı Tek");
+
+            //aşağıdaki şekilde de kullanılabilir.
+            //  string sonuc = (sayı % 2==0) ?"Sayı Çift":"Sayı Tek" ;
+            //  Console.WriteLine(sonuc);
+
+
+            // yukarıdaki ternary yapısının if else ile çözümü
+            //if (sayı % 2 == 0)
+            //    sonuc = "Sayı Çift";
+            //else
+            //    sonuc = "Sayı Tek";
+        }
+
+        private static void ifelse4()
+        {
+            double gelir = 7000, vergiKesintisi = 0, netGelir = 0; //hesaplanacak
+
+            if (gelir <= 5000)
+                vergiKesintisi = gelir * .25;
+            // 5000 ile 8000 arassındaysa
+            else if (gelir <= 8000)
+                vergiKesintisi = gelir * .30;
+            // 8000 ile 12000 arasındaysa
+            else if (gelir <= 12000)
+                vergiKesintisi = gelir * .35;
+            //12000 den büyükse
+            else
+                vergiKesintisi = gelir * .40;
+
+            netGelir = gelir - vergiKesintisi;
+            Console.WriteLine($"Kesinti Tutarı :{vergiKesintisi}\nNet Gelir :{netGelir}");
+        }
+
+        private static void ifelse3()
+        {
+            //Öğrencinin vize ve final notunu klavyeden aldıktan sonra ortalamasını hesaplayıp,
+            //Daha sonra; Final notu 50 ve üzeri ise ve ortalaması 60’ın üzerinde ise GEÇTİ,
+            //değilse KALDI yazacak bir C# programı.
+            //Parse metodu string değişken yada ifadeleri sayıya dönüştürür.(int, double, float vs..)
+            float vize = 0, final = 0, ortalama = 0;
+            Console.WriteLine("Vize Notunu Giriniz");
+            vize = float.Parse(Console.ReadLine());
+            Console.WriteLine("Final Notunu Giriniz");
+            final = float.Parse(Console.ReadLine());
+            ortalama = vize * .40f + final * .60f;
+
+            Console.WriteLine($"Sınav Ortalamanız : {ortalama}");
+            if (ortalama >= 60 && final >= 50)
+                Console.WriteLine("Geçtiniz");
+            else
+                Console.WriteLine("!!!!!Kaldınız!!!!!");
+        }
+
+        private static void ifelse2()
+        {
+            //bloklu if else yapısı
+            //gno >=3 olanlara +4 kredi  ve harç tutarı %50 indirim
+            //değilse +2 kredi harç tutarı değişmeyecek.
+            float gno = 1.1f;
+            int kredi = 24;
+            float harcTutari = 450;
+
+            if (gno >= 3)//true ise aşağıdaki kodlar çalışır(else kadar)
+            {
+                kredi += 4;
+                harcTutari /= 2;
+                Console.WriteLine($"Krediniz Artırıldı. Yeni krediniz : {kredi}");
+            }
+            //if deki mantıksal sınama false olurs çalışır
+            //kredi 3'ten küçük ama 2'den de küçük olanlara kredi artırımı olmasın
+            else
+            { //nested if else
+                if (gno >= 2 && gno < 3)//&& gno < 3 bu ifadeye gerek yok. çünkü if bloğunda 3'ten küçük olduğu belli oldu. tekrar 3'ten küçük mü diye sormaya gerek yok.
+                {
+                    kredi += 2;
+                    Console.WriteLine($"Krediniz Artırıldı. Yeni krediniz : {kredi}");
+                }
+                else
+                {
+                    Console.WriteLine("Ortalamanız 2'nin altında olduğundan kredi artırımı yapılmadı...");
+                    Console.WriteLine($"Krediniz  : {kredi}");
+
+                }
+
+            }
+            Console.WriteLine($"Harç Tutarınız : {harcTutari}");
+        }
+
+        private static void ifelse1()
+        {
+            double gno = 2.5;
+            bool sonuc = gno > 3;
+            //float ve decimal değerler değişkene aktarılırken f yada m ekleri kullanılır.
+            // mantıksal ifade = 12 > 8;
+            //mantıksal sonuç = true yada false
+            if (gno > 3)//20 satıra true olursa girer
+                Console.WriteLine("Başarılısınız");
+            else
+                Console.WriteLine("Başarılı değilsiniz");
+            Console.WriteLine("Karşılaştırma işlemi bitti..");
+        }
+    }
+}
---
+++ Makina_2_1/Makina.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makina_2_Hafta_Ders1
+{
+    class Makina
+    {
+        //c# nesne yönelik programlama olduğundan herşey bir clas içinde yer almalıdrı.
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Merhaba Makina Bölümü");
+            //Console.WriteLine("Kütahya Dumlupınar Üniversitesi");
+            //Console.WriteLine("Mühendislik Fakültesi");
+        //------------------------------
+            //değişken tanımlama
+            string mesaj;
+            //ilk değeri verilmemiş bir değişken kullanılırsa hata oluşur
+            //bir değişkene değer atama
+            mesaj = "Değişkenleri kullanmak için lütfen değer atayın";
+            mesaj = "";//değişkenin içini boşaltır
+            mesaj = null; //null hiçbirşey demektir.
+            Console.WriteLine(mesaj);
+            //------------------------------
+            //bir değişken tipinin maximum ve minumum değerini bulmak için "MinValue" ve "MaxValue" metodları kullanılır. :N simgesi sayıların binlik ayraçlı bir şekilde gösterilmesini sağlar
+            //Console.WriteLine("int tipinin en küçük değeri {0:N}", int.MinValue);
+            //Console.WriteLine("int tipinin en büyük değeri {0:N}",int.MaxValue);
+            //Console.WriteLine("fark :{0}",202013121001-int.MaxValue);
+            //----------------------
+            long ogrno = 202013121001; //long tipi tamsayı tipidir
+            string adSoyad = "İSMAİL CEM"; //string metin(yazı) veri tipidir
+            double gno = 3.2;
+            // 202013121001 nolu İSMAİL CEM'in genel not ortalaması : 3.2'dir. 
+
+            //Console.WriteLine("Öğrencinin numarası :"+ogrno);
+            //Console.WriteLine("Öğrencinin numarası :{0}",ogrno);
+
+            //Console.WriteLine($"Öğrencinin numarası :{ogrno}");
+
+            //Console.WriteLine($"{ogrno} nolu {adSoyad}'in genel not ortalaması : {gno}'dir.");
+            //----------------
+            int x = 20;
+            int y = 25;
+            int toplam = x + y;
+            // bir değişkenin veri tipini öğrenmek için GetType() metodu kullanılır
+           // Console.WriteLine(toplam.GetType());
+            //Console.WriteLine($"{x} + {y} = {toplam}");
+            //Console.WriteLine($"{x} + {y} = {x + y}");
+            //Console.WriteLine("x ve y'nin toplamı :" + (x + y));
+            //sayısal verileri yazdırırken 1000'lik ayracı kullanmak N parametresini kullanırız
+            double maas = 9250.796;
+            float ucret = 12570.48f;
+            decimal odeme = 1254547.7898701M;
+            //Console.WriteLine($"Ödenecek Ücret : {ucret:N1}");
+            //Console.WriteLine($"Yapılacak ödeme : {odeme:N3}");
+            //Console.WriteLine($"Ödenecek Maaş : {maas:N2}");
+            //Console.WriteLine($"Ödenecek Ücret : {ucret:C1}");
+            //Console.WriteLine($"Yapılacak ödeme : {odeme:C3}");
+            //Console.WriteLine($"Ödenecek Maaş : {maas:C2}");
+//------------------------------
+            //Mantıksal veri tipleri
+            int x1 = 20;
+            int x2 = 25;
+            int t = 100;
+            bool b1 = true;
+            bool b2 = 10 > 8;
+            bool b3 = (10 * 5 >= 20 * 3);
+            // veya sembolu ||    alt gr+z tuşunun solundaki tuşa birlikte basılır
+            // ve   sembolu &&  shift + 6 tuşlarına birlikte basılır
+            // eşitlik sembolu == (iki tane eşit simgesi)
+            bool b4 = (x1 == x2) || (x1 * x2 > t);
+           //   \n satır atlatır
+            //Console.WriteLine("b1 : " + b1);
+            //Console.WriteLine("b2 : " + b2);
+            //Console.WriteLine("b3 : " + b3);
+            //Console.WriteLine("b4 : " + b4);
+           // yukarıdaki 4 satır yerine aşağıdaki gibi de yazabiliriz
+           // Console.WriteLine($"b1 : {b1}\nb2 : {b2}\nb3 : {b3}\nb4 : {b4}");
+            //  --------------------------------
+            //   Console.ReadLine() kullanımı........Console.ReadLine();//enter tuşu basılana kadar bekler
+
+            string AdSoyad;
+            string Bolum;
+            Console.WriteLine("Ad Soyad Giriniz :");
+            AdSoyad = Console.ReadLine();
+            Console.WriteLine("Bölümünüzü Giriniz :");
+            Bolum = Console.ReadLine();
+
+            Console.WriteLine("Girdiğiniz Değerler :");
+            Console.WriteLine($"Ad Soyad : {AdSoyad}");
+            Console.WriteLine($"Bölümünüz : {Bolum}");
+            Console.ReadLine();
+        }
+
+    }
+}

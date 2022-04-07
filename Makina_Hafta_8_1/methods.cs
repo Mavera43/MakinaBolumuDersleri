878b84b3d717ac286b4970b4d9a7b7522c3e513e
Senol Demirci <99175370+Mavera43@users.noreply.github.com>
2022-04-07 11:59:03 +0300

    Add files via upload

---
+++ Makina_Hafta_8_1/methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makina_Hafta_8_1
+{
+    class methods
+    {
+        //çemberin alanını hesaplayan method/fonksiyon/yöntem yazalım. Bu metod bize alanı geri döndürsün
+
+        //parametre almayan ve geri değer döndürmeyen methot tipi
+        void cemberAlaniHesapla()
+        {
+            Console.WriteLine("İki değerin toplamı : " + (10+25));
+            //kod bloğu
+        }
+
+        //parametre alan fakat geri değer döndürmeyen methot tipi
+        void cemberAlaniHesapla(double yariCap)
+        {
+
+        }
+        public double cemberAlaniHesapla(double yariCap, double pi)
+        {
+            return 0;
+        }
+        //derleyici, parametreleri ve sayıları birbirinden farklı olmak koşulu ile aynı metod isimlerine izin verir.
+        //parametre almayan fakat geri değer döndüren methot tipi
+        double cemberAlaniHesapla2()
+        {
+            return 0;
+        }
+
+        static void Main(string[] args) //başlatıcı metod.
+        {
+
+        }
+    }
+}

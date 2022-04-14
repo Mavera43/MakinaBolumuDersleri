181e1b4cd865dcb1930e603823c905ea79c2453f
Senol Demirci <99175370+Mavera43@users.noreply.github.com>
2022-04-14 11:24:44 +0300

    Add files via upload

---
+++ Makina_Hafta_9_1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makina_Hafta_9_1
+{
+    class Program
+    {
+       public int makina = 10;
+       private string dpu = "Kütahya Dumlupınar Üniversitesi";
+        // bool tipli değişkenlerin varsayılan değeri false'dur.
+       bool intvalesitmi(int val1, int val2)
+        {
+            /*
+            if (val1.Equals(val2))
+                return true;
+            else
+                return false;*/
+
+            bool retval;
+            /* if (val1.Equals(val2))
+                 retval = true;
+             else
+                 retval = false;*/
+            //ternary operatörü //üçlü operatör
+            retval = (val1.Equals(val2)) ? true : false;
+            retval = (val1.Equals(val2));
+            return retval;
+
+        }
+        private static void esitmiMetodu()
+        {
+            Program prg = new Program();
+            bool sonuc = prg.intvalesitmi(12, 12);
+            if (sonuc)
+                Console.WriteLine("Sayılar eşit");
+            else
+                Console.WriteLine("Sayılar farklı");
+        }
+        private static bool doublevalesitmi(double val1, double val2)
+        {
+            if (val1.Equals(val2))
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.intvalesitmi(4, 8);
+            p.makina = 20;
+            p.makina = 30;
+            p.dpu = "Makina Mühendisliği";
+
+            p.intvalesitmi(1, 2);
+            bool sonuc= doublevalesitmi(98.9, 98.9);
+            Console.WriteLine(doublevalesitmi(98.9, 98.9));
+            if(doublevalesitmi(1.1,1.2))
+            {
+
+            }
+            else
+            {
+
+            }
+            // intvalesitmi(10.5, 98.9);
+            //esitmiMetodu();
+
+        }
+
+      
+
+        // seçili olan satırları metoda dönüştürür.  CRTL + R + M 
+        
+    }
+}
---
+++ Makina_Hafta_9_2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makina_Hafta_9_2
+{
+    class Program
+    {
+        //değişken sayıda parametre alan metodlar / params parametresi kullanımı
+  
+
+        static void ekle(out int sayi)
+        {
+            sayi = 55;
+            sayi += 5;
+            
+        }
+
+        static void Outhesapla(int[] sayilar, out double toplam, out double ortalama )
+        {
+            toplam = 0;ortalama = 0;
+            foreach (var sayi in sayilar)
+            {
+                toplam += sayi;
+            }
+            //bir dizi(array) eleman sayısını bulmak için Count yöntemi kullanılır
+            ortalama = toplam / sayilar.Count();
+        }
+
+        static void Main(string[] args)
+        {
+            double toplam;
+            double ortalama;
+            int[] dizi = new int[] { 1, 8, 7, 6, 5, 70, 55 };
+            Outhesapla(dizi, out toplam, out ortalama);
+            Console.WriteLine($"Sayıların Toplamı : {toplam}\nSayıların Ortalaması : {ortalama:N2}");
+
+
+            //int sayi;
+            //ekle(out sayi);
+            //Console.WriteLine($"sayı değeri :{sayi}");
+          //  --------------------------------
+          /*  Console.WriteLine("sayı değeri :{0}",sayi);
+            Console.WriteLine("sayı değeri :"+sayi);*/
+
+            /*  sonuc = topla();
+              sonuc = topla(12, 4);
+              sonuc = topla(55, 68, 90, 100);*/
+            //sonuc= hesapla('*', 8, 4, 2);
+            // sonuc = hesapla('-', 8, 4, 2);
+
+        }
+
+
+        static int topla(params int[] sayilar)
+        {
+            int toplamaSonucu = 0;    ///    int < double
+            if (sayilar.Length == 0)
+                return 0;
+            foreach (var sayi in sayilar)
+            {
+                toplamaSonucu += sayi;
+            }
+            return toplamaSonucu;
+        }
+
+        static int hesapla(char islemci, params int[] sayilar)
+        {
+            //değer döndüren metotlarda en az 1 tane return olması gerekir
+            int toplam = 0;
+            if (islemci == '+')
+            {
+                foreach (var sayi in sayilar)
+                {
+                    toplam += sayi;
+                }
+                return toplam;
+            }
+            else if (islemci == '*')
+            {
+                toplam = 1;
+                foreach (var sayi in sayilar)
+                {
+                    toplam *= sayi;
+                }
+                return toplam;
+            }
+            else
+                return 0;
+        }
+        static int sum(int x, int y)
+        {
+            return 1;
+        }
+        static int sum(int x, int y, int z)
+        {
+            return 1;
+        }
+    }
+}
